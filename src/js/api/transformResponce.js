51da924e4ed84da8df91d11ee4eb0a8355b54cd6
Rozali-Ab <rozali_ab@outlook.com>
2024-03-19 02:55:47 +0300

    chores
    complete mvp

---
+++ src/js/api/transformResponce.js
@@ -0,0 +1,23 @@
+export const transformResponce = (data) => {
+  const weather = {
+    city: (data && data.name) || '',
+    description: (data.weather && data.weather[0] && data.weather[0].description) || '',
+    clouds: (data.clouds && `${data.clouds.all} %`) || '',
+    temp: `${(data.main.temp).toFixed(0)}°`,
+    feelsLike: `${(data.main.feels_like).toFixed(0)}°`,
+    visibility: `${convertToKm(data.visibility)} км` || '',
+    wind: `${(data.wind.speed).toFixed(0)} м/с` || '',
+    pressure: `${convertPressureToMmHg(data.main.pressure)} мм рт. ст.` || '',
+    id: data.id || '',
+  }
+  //console.log(weather)
+  return weather;
+};
+
+const convertToKm = (m) => {
+  return m/1000;
+};
+
+const convertPressureToMmHg = (pressure) => {
+  return (pressure * 0.75006).toFixed(0);
+};
---
+++ src/js/store/localStore.js
@@ -0,0 +1,46 @@
+export const localStore = {
+  location: 'location',
+  currentWeather: 'current weather',
+  cityList: 'city list',
+}
+
+export const localStorageModule = {
+  saveData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+  getData(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  },
+  clearData(key) {
+    localStorage.removeItem(key);
+  },
+};
+
+export const saveLocationToLocalStorage = (location) => {
+  localStorageModule.saveData(localStore.location, location);
+};
+
+export const getLocationFromLocalStorage = () => {
+  return localStorageModule.getData(localStore.location);
+};
+
+export const saveWeatherToLocalStorage = (weather) => {
+  localStorageModule.saveData(localStore.currentWeather, weather);
+};
+
+export const getWeatherFromLocalStorage = () => {
+  return localStorageModule.getData(localStore.currentWeather);
+};
+
+export const saveCityListToLocalStorage = (cityList) => {
+  if (cityList.length === 0) {
+    localStorageModule.clearData(localStore.cityList);
+  }
+  localStorageModule.saveData(localStore.cityList, cityList);
+};
+
+export const getCityListFromLocalStorage = () => {
+  return localStorageModule.getData(localStore.cityList);
+};
+
