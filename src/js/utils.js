f5b6d72fdec1611cb0ca19c1cc52e78858723ee2
Rozali-Ab <rozali_ab@outlook.com>
2024-03-19 02:57:45 +0300

    add utils

---
+++ src/js/utils.js
@@ -0,0 +1,38 @@
+export const debounce = (fn, delay) => {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+    return new Promise((resolve) => {
+      timer = setTimeout(() => {
+        resolve(fn(...args));
+      }, delay);
+    });
+  };
+};
+
+const convertToKm = (m) => {
+  return m/1000;
+};
+
+const convertPressureToMmHg = (pressure) => {
+  return (pressure * 0.75006).toFixed(0);
+};
+
+export const transformResponce = (data) => {
+  const weather = {
+    city: (data && data.name) || '',
+    description: (data.weather && data.weather[0] && data.weather[0].description) || '',
+    clouds: (data.clouds && `${data.clouds.all} %`) || '',
+    temp: {
+      celsius: `${(data.main.temp).toFixed(0)}°`,
+      feelsLike: `${(data.main.feels_like).toFixed(0)}°`,
+    },
+    visibility: `${convertToKm(data.visibility)} км` || '',
+    wind: `${(data.wind.speed).toFixed(0)} м/с` || '',
+    pressure: `${convertPressureToMmHg(data.main.pressure)} мм рт. ст.` || '',
+  }
+  console.log(weather)
+  return weather;
+};
+
