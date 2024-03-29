a1ab5573ef844b7affe603d90ee48e30b8b9631c
Rozali-Ab <rozali_ab@outlook.com>
2024-03-30 01:02:37 +0300

    add class Store

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,15 +1,99 @@
-import { getWeatherFromLocalStorage, getCityListFromLocalStorage, getLocationFromLocalStorage } from './localStore.js';
+import { getDataByCity, getDataByLocation } from "../api/api";
 
-export const store = {
-  currentWeather: {},
-  currentGeo: {},
-  cityList: [],
-};
+export default class Store {
+  constructor() {
+    this._currentWeather = {};
+    this._cityList = [];
+    this._isLoading = false;
 
-export const initStore = async () => {
-  store.currentWeather = getWeatherFromLocalStorage() || {};
-  store.currentGeo = getLocationFromLocalStorage() || {};
-  store.cityList = getCityListFromLocalStorage() || [];
+    this.getDataFromLocalStorage();
+  }
 
-  return store;
-};
+  getCurrentWeather() {
+    return this._currentWeather;
+  }
+
+  setCurrentWeather(weather) {
+    this._currentWeather = weather;
+  }
+
+  getCityList() {
+    return this._cityList;
+  }
+
+  setCityList(cityList) {
+    this._cityList = cityList;
+  }
+
+  getIsLoading() {
+    return this._isLoading;
+  }
+
+  setIsLoading(loading) {
+    this._isLoading = loading;
+  }
+
+
+  getDataFromLocalStorage() {
+    const storedWeather = localStorage.getItem('currentWeather');
+    if (storedWeather) {
+      this.setCurrentWeather((JSON.parse(storedWeather)));
+    }
+
+    const storedList = localStorage.getItem('cityList');
+    if (storedList) {
+      this.setCityList(JSON.parse(storedList));
+    }
+  }
+
+  setCurrentWeatherToLocalStorage() {
+    localStorage.setItem('currentWeather', JSON.stringify(this.getCurrentWeather()));
+  }
+
+  setCityListToLocalStorage() {
+    localStorage.setItem('cityList', JSON.stringify(this.getCityList()));
+  }
+
+  async getWeatherByLocation(location) {
+    try {
+      const weather = await getDataByLocation(location);
+      if (weather) {
+        this.setCurrentWeather(weather);
+        this.setCurrentWeatherToLocalStorage();
+        return weather;
+      }
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async getWeatherByCityName(cityName) {
+    this.setCurrentWeather(await getDataByCity(cityName));
+    this.setCurrentWeatherToLocalStorage();
+  }
+
+  addCityToList(city) {
+    this.getCityList().push(city);
+    this.setCityListToLocalStorage(this.getCityList());
+  }
+
+  deleteCityFromListById(id) {
+    const index = this.getCityList().findIndex(city => city.id === id);
+
+    if (index !== -1) {
+      const updatedList = this.getCityList().slice();
+      updatedList.splice(index, 1);
+      this.updateCityList(updatedList);
+    }
+  }
+
+  updateCurrentWeather(currentWeather) {
+    this.setCurrentWeather(currentWeather);
+    this.setCurrentWeatherToLocalStorage();
+  }
+
+  updateCityList(updatedList) {
+    this.setCityList(updatedList);
+    this.setCityListToLocalStorage(updatedList);
+  }
+}
